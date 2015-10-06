bbfd9702812397eb8e7ba2b7f95ad97bc06c680c
Michael Faller <mfaller@gmail.com>
2015-10-06 11:29:45 -0400

    Added comments for Person

--- Cemetery/src/cs/softengine/Person.java
+++ Cemetery/src/cs/softengine/Person.java
@@ -25,59 +25,114 @@ public class Person {
     }
     */
 
-    // set and get methods...
-    public void setFname(String fname) {
+    /**
+     * Set first name
+     * @param fname first name
+     */
+    public void setFirstName(String fname) {
         this.fname = fname;
     }
 
-    public String getFname() {
+    /**
+     * Get first name
+     * @return first name
+     */
+    public String getFirstName() {
         return fname;
     }
 
-    public void setLname(String lname) {
+    /**
+     * Set last name
+     * @param lname last name
+     */
+    public void setLastName(String lname) {
         this.lname = lname;
     }
 
-    public String getLname() {
+    /**
+     * Get last name
+     * @return lname last name
+     */
+    public String getLastName() {
         return lname;
     }
 
+    /**
+     * Set address line 1
+     * @param address1 address line 1
+     */
     public void setAddress1(String address1) {
         this.address1 = address1;
     }
 
+    /**
+     * Get address line 1
+     * @return address1
+     */
     public String getAddress1() {
         return address1;
     }
 
+    /**
+     * Set address line 2
+     * @param address2 address line 2
+     */
     public void setAddress2(String address2) {
         this.address2 = address2;
     }
 
+    /**
+     * Get address line 2
+     * @return address2 address line 2
+     */
     public String getAddress2() {
         return address2;
     }
 
+    /**
+     * Set the city
+     * @param city city
+     */
     public void setCity(String city) {
         this.city = city;
     }
 
+    /**
+     * Get the city
+     * @return city
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     * Set the state
+     * @param state state
+     */
     public void setState(String state) {
         this.state = state;
     }
 
+    /**
+     * Get state
+     * @return state
+     */
     public String getState() {
         return state;
     }
 
+    /**
+     * Set zip code
+     * @param zip zip code
+     */
     public void setZip(String zip) {
         this.zip = zip;
     }
 
+    /**
+     * Get zip code
+     * @return zip code
+     */
     public String getZip() {
         return zip;
     }
