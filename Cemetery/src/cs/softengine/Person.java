e5d6e7b7f8bec56d94726ce0f2793401e39df8ed
djjkobe <djjkobe@gmail.com>
2015-10-06 11:11:51 -0400

    Finish basic set and get method of Person class.

--- Cemetery/src/cs/softengine/Person.java
+++ Cemetery/src/cs/softengine/Person.java
@@ -26,4 +26,60 @@ public class Person {
     */
 
     // set and get methods...
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
 }
