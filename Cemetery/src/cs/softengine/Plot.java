5d345118289bbe3487bb69e24cbba162e047e317
Michael Faller <mfaller@gmail.com>
2015-10-11 19:57:03 -0400

    implemented accessor and mutator methods for Plot and compareTo based on ID num

--- Cemetery/src/cs/softengine/Plot.java
+++ Cemetery/src/cs/softengine/Plot.java
@@ -9,22 +9,21 @@ import java.util.Date;
  */
 public class Plot implements Comparable<Plot> {
     String section; // residing section name
+    int id; // plot identifier number
     Person interred; // interred person
     Person owner; // contact person, also person fiscally responsible for plot
     Date burial; // burial date
     Date purchased; // purchase date
-    float moneyDue; // if not 0, person owes this much
     boolean vacant; // is the plot vacant/not vacant
     boolean ready; // is the plot ready for use or not ready
+    int moneyDue; // if not 0, person owes this much IN CENTS (for accuracy)
 
-    // we need some sort of unique identifier for a plot
-    // int identifier;
     /**
      * Constructs an empty plot
      */
     public Plot() {
-        System.out.println("Default constructor: Empty plot.");
         section = null;
+        id = -1;
         interred = new Person();
         owner = new Person();
         burial = new Date();
@@ -32,26 +31,166 @@ public class Plot implements Comparable<Plot> {
         moneyDue = 0;
         vacant = true;
         ready = true;
-
     }
-
-    // TODO add constructor for plot with provided info
-
-    // TODO add methods
 
     /**
-     * Get a plot
-     * @param p a person
-     * @return Plot, null if
+     * Constructs a basic plot
+     * @param section name
+     * @param id number
      */
-    public Plot get(Person p){
-        // TODO
-        return null;
+    public Plot(String section, int id) {
+        this.section = section;
+        this.id = id;
     }
 
-    public void set(Person p){
-        // TODO
+    /**
+     * Get plot ID number
+     * @return id number
+     */
+    public int getID() {
+        return id;
     }
+
+    /**
+     * Set plot ID number
+     * @param id new id number
+     */
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Get section name
+     * @return section name
+     */
+    public String getSection() {
+        return section;
+    }
+
+    /**
+     * Set section name
+     * @param s section name
+     */
+    public void setSection(String s) {
+        section = s;
+    }
+
+    /**
+     * Get interred person
+     * @return interred person
+     */
+    public Person getInterred() {
+        return interred;
+    }
+
+    /**
+     * Set interred person
+     * @param p interred person
+     */
+    public void setInterred(Person p) {
+        interred = p;
+    }
+
+    /**
+     * Get owner of plot
+     * @return owner of plot
+     */
+    public Person getOwner() {
+        return owner;
+    }
+
+    /**
+     * Set owner of plot
+     * @param p owner of plot
+     */
+    public void setOwner(Person p) {
+        owner = p;
+    }
+
+    /**
+     * Get burial date
+     * @return burial date
+     */
+    public Date getBurialDate() {
+        return burial;
+    }
+
+    /**
+     * Set burial date
+     * @param d burial date
+     */
+    public void setBurialDate(Date d) {
+        burial = d;
+    }
+
+    /**
+     * Get purchase date
+     * @return purchase date
+     */
+    public Date getPurchasedDate() {
+        return purchased;
+    }
+
+    /**
+     * Set purchase date
+     * @param d purchase date
+     */
+    public void setPurchasedDate(Date d) {
+        purchased = d;
+    }
+
+    /**
+     * Is the plot vacant
+     * @return true, if the plot is vacant
+     *         false, if the plot is not vacant
+     */
+    public boolean isVacant() {
+        return vacant;
+    }
+
+    /**
+     * Set the plot to vacant or not vacant
+     * @param b true, if the plot is vacant
+     *          false, if the plot is not vacant
+     */
+    public void setVacant(boolean b) {
+        vacant = b;
+    }
+
+    /**
+     * Is the plot ready
+     * @return true, if the plot is ready
+     *         fakse, if the plot is not ready
+     */
+    public boolean isReady() {
+        return ready;
+    }
+
+    /**
+     * Set the plot to ready or not ready
+     * @param b true, if the plot is ready
+     *          false, if the plot is not ready
+     */
+    public void setReady(boolean b) {
+        ready = b;
+    }
+
+    /**
+     * Get the amount of money due
+     * @return the money due in cents
+     */
+    public int getMoneyDue() {
+        return moneyDue;
+    }
+
+    /**
+     * Set the amount of money due
+     * @param m the money due in cents
+     */
+    public void setMoneyDue(int m) {
+        moneyDue = m;
+    }
+
     /**
      * Compare a plot to another plot
      * @param p a plot
@@ -61,9 +200,11 @@ public class Plot implements Comparable<Plot> {
      */
     @Override
     public int compareTo(Plot p) throws NullPointerException {
-        // how do we compare plots? we need a unique identifier TODO
-        // this will come in handy when we are searching for plots or people
-        return 0;
+        if (p == null) {
+            throw new NullPointerException();
+        }
+
+        return p.getID() - id;
     }
 
     /**
@@ -73,7 +214,7 @@ public class Plot implements Comparable<Plot> {
      */
     public boolean equals(Object o) throws NullPointerException {
         if (o == null) {
-            return false;
+            throw new NullPointerException();
         }
 
         if (getClass() != o.getClass()) {
