2479fdcf9496671189d3108584e3e1e3481b1dd6
daytimebat <xkwanghttp@gmail.com>
2015-10-11 21:59:05 -0400

    Add code to PlotTest

--- Cemetery/src/tests/PlotTest.java
+++ Cemetery/src/tests/PlotTest.java
@@ -1,9 +1,32 @@
 package tests;
 import cs.softengine.Plot;
 import org.junit.Test;
+import static org.junit.Assert.*;
 /**
  * Created by jiajiedang on 10/11/15.
  */
 public class PlotTest {
+
     Plot p;
+
+    /* Tests that Plot.getID() correctly gets the id
+         of the Plot class.
+     */
+    @Test
+    public void testGetID() throws Exception {
+        p = new Plot();
+        assertEquals("ID must be -1, but is " + p.getID(), p.getID(), "-1");
+    }
+
+    /* Tests that Plot.setID() correctly sets the id
+         of the Plot class.
+     */
+    @Test
+    public void testSetID() throws Exception {
+        p = new Plot();
+        p.setID(1);
+        assertEquals("ID must be 1, but is " + p.getID(), p.getID(), "1");
+    }
+
+
 }
