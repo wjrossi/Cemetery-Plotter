7c41725c7ab50a9bc8a04008be950f733eadcc08
daytimebat <xkwanghttp@gmail.com>
2015-10-26 15:48:42 -0400

    add test methods

--- Cemetery/src/tests/CemeteryTest.java
+++ Cemetery/src/tests/CemeteryTest.java
@@ -1,8 +1,52 @@
 package tests;
 
+import cs.softengine.Cemetery;
+import org.junit.Test;
+
 /**
  * Tests for Cemetery
  */
 public class CemeteryTest {
     // ???
+    Cemetery c;
+
+    @Test
+    public void testAdd(){
+        c = new Cemetery();
+    }
+
+    @Test
+    public void testRemove(){
+
+    }
+
+    @Test
+    public void testSet(){
+
+    }
+
+    @Test
+    public void testGetSections(){
+
+    }
+
+    @Test
+    public void testSetSections(){
+
+    }
+
+    @Test
+    public void testGetPlots(){
+
+    }
+
+    @Test
+    public void testGetInterredPeople(){
+
+    }
+
+    @Test
+    public void testGetPeople(){
+        
+    }
 }
