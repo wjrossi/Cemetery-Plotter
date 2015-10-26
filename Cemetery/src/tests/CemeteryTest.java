6886724b28296a19f6fb0718ac263f41270d111a
daytimebat <xkwanghttp@gmail.com>
2015-10-26 16:12:09 -0400

    add some comments

--- Cemetery/src/tests/CemeteryTest.java
+++ Cemetery/src/tests/CemeteryTest.java
@@ -10,43 +10,75 @@ public class CemeteryTest {
     // ???
     Cemetery c;
 
+    /*
+     * Tests that Cemetery.add() correctly add a section to Cemetery class.
+     */
     @Test
     public void testAdd(){
         c = new Cemetery();
     }
 
+    /*
+     * Tests that Cemetery.remove() correctly remove a section to Cemetery class.
+     */
     @Test
     public void testRemove(){
 
     }
 
+    /*
+     * Tests that Cemetery.get() correctly get a section to Cemetery class.
+     */
     @Test
-    public void testSet(){
+    public void testGet(){
 
     }
 
+    /*
+     * Tests that Cemetery.getSections() correctly Get ArrayList of sections in Cemetery class.
+     */
     @Test
     public void testGetSections(){
 
     }
 
+    /*
+     * Tests that Cemetery.setSections() correctly Set the ArrayList of sections in the Cemetery class.
+     */
     @Test
     public void testSetSections(){
 
     }
 
+    /*
+     * Tests that Cemetery.getPlots() correctly Get list of all plots in Cemetery class.
+     */
     @Test
     public void testGetPlots(){
 
     }
 
+    /*
+     * Tests that Cemetery.getInterredPeople() correctly Get list of all interred people in Cemetery class.
+     */
     @Test
     public void testGetInterredPeople(){
 
     }
 
+    /*
+     * Tests that Cemetery.getPeople() correctly Get list of all (non-interred) people in Cemetery class.
+     */
     @Test
     public void testGetPeople(){
-        
+
+    }
+
+    /*
+     * Tests that Cemetery.toString() correctly Write this cemetery to a string (for saving).
+     */
+    @Test
+    public void testToString(){
+
     }
 }
