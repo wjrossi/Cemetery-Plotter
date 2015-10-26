91f9eac4b53545b88009da4f4fe19d2c6fe172a7
Michael Faller <mfaller@gmail.com>
2015-10-26 11:30:06 -0400

    new branch for gui layout with additions to plot panel

--- Cemetery/src/cs/softengine/CemeteryPlotterPlot.java
+++ Cemetery/src/cs/softengine/CemeteryPlotterPlot.java
@@ -4,12 +4,18 @@ import javax.swing.*;
 import javax.swing.border.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Content pane for plot info
  */
 public class CemeteryPlotterPlot implements ActionListener, ItemListener {
     private JPanel plotPanel;
+    private SimpleDateFormat sdf = new SimpleDateFormat("MM/DD/YYYY");
+    private NumberFormat nf = NumberFormat.getCurrencyInstance();
 
     /**
      * Constructs a content pane for plot info
@@ -40,6 +46,44 @@ public class CemeteryPlotterPlot implements ActionListener, ItemListener {
         panel.setBorder(titledBorder);
 
         // add things to panel
+
+        // create labels
+        JLabel sectionLabel = new JLabel("Section:");
+        JLabel plotIDLabel = new JLabel("Plot ID#:");
+        JLabel burialDateLabel = new JLabel("Burial Date:");
+        JLabel purchasedDateLabel = new JLabel("Purchased Date:");
+        JLabel moneyDueLabel = new JLabel("Amount Owed:");
+
+        // create text fields
+        JTextField sectionField = new JTextField();
+        JTextField plotIDField = new JTextField();
+        JFormattedTextField burialDateField = new JFormattedTextField(sdf);
+        JFormattedTextField purchasedDateField = new JFormattedTextField(sdf);
+        JFormattedTextField moneyDueField = new JFormattedTextField(nf);
+
+        // set labels to text fields
+        sectionLabel.setLabelFor(sectionField);
+        plotIDLabel.setLabelFor(plotIDField);
+        burialDateLabel.setLabelFor(burialDateField);
+        purchasedDateLabel.setLabelFor(purchasedDateField);
+        moneyDueLabel.setLabelFor(moneyDueField);
+
+        // create status check boxes
+        JCheckBox vacantCheckBox = new JCheckBox("Vacant:", false);
+        JCheckBox readyCheckBox = new JCheckBox("Ready:", false);
+
+        // create edit button
+        JButton editButton = new JButton("Edit"); // when clicked will unlock text fields and allow changes
+
+
+        // create sub-panels
+
+
+        // add items to sub-panels
+
+
+        // add sub-panels to main panel
+
 
         return panel;
     }
