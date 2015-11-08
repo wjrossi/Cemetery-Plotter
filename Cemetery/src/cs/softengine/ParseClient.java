b14e4367bbe1a5b376fd24f96cf4eedec96246bf
Daniel Kindler <DanielKindler@Dans-Mac.local>
2015-11-08 17:46:32 -0500

    add uploading

--- Cemetery/src/cs/softengine/ParseClient.java
+++ Cemetery/src/cs/softengine/ParseClient.java
@@ -2,13 +2,14 @@ package cs.softengine;
 import org.parse4j.*;
 import org.parse4j.callback.*;
 import java.io.File;
+import java.nio.charset.*;
 
 /**
  * Created by DanielKindler on 11/8/15.
  */
 public class ParseClient {
-
     private static String FILE_OBJ_ID = "rQupr9UOFz";
+    private static ParseObject saveObject = ParseObject.createWithoutData("Save", FILE_OBJ_ID);
     private static String APP_ID = "PMjsWvJ0jF5SvX70J7n19JaJ40SwsRSDbSz2wedp";
     private static String APP_REST_ID = "eZZDX9qchn88zkLhh02Q8xRToOTzlfrSVBEzs7qM";
 
@@ -19,11 +20,24 @@ public class ParseClient {
 
     /**
      * Retrieve file with name from server
-     * @param fileName the name of the file
      * @return boolean if user logged in successfully or not
      */
-    public File getFile(String fileName) {
-        return null;
+    public static File getFile() {
+
+        ParseFile file = (ParseFile) saveObject.get("file");
+        File f = null;
+        file.getDataInBackground(new GetDataCallback() {
+            public void done(byte[] data, ParseException e) {
+                if (e == null) {
+                    String str = new String(data, StandardCharsets.UTF_8);
+                    f = new File("cemetery.db", str);
+                } else {
+                    // throw error
+                }
+            }
+        });
+
+        return f;
     }
 
     /**
@@ -32,15 +46,11 @@ public class ParseClient {
      * @return boolean if file saved successfully or not
      */
     public boolean saveFile(File inFile) throws ParseException {
-
         String str = inFile.toString();
         byte[] data = str.getBytes();
-
         ParseFile file = new ParseFile(inFile.getName(), data);
-        ParseObject saveObj = ParseObject.createWithoutData("File", FILE_OBJ_ID);
-        saveObj.add("file", file);
-
-        saveObj.saveInBackground();
+        saveObject.put("file", file);
+        saveObject.saveInBackground();
 
         return true;
     }
