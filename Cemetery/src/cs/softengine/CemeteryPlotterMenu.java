847e5919e023f24b021af47fa04c0451a80d9daf
Michael Faller <mfaller@gmail.com>
2015-11-16 16:34:51 -0500

    changed var name and unified save and save as conventions

--- Cemetery/src/cs/softengine/CemeteryPlotterMenu.java
+++ Cemetery/src/cs/softengine/CemeteryPlotterMenu.java
@@ -154,7 +154,7 @@ public class CemeteryPlotterMenu extends CemeteryPlotter implements ActionListen
      */
     public void save() {
         if (cemetery.isModified()) { // unsaved changes?
-            int open = JOptionPane.showOptionDialog(cemeteryPlotterFrame.getFrame(),
+            int save = JOptionPane.showOptionDialog(cemeteryPlotterFrame.getFrame(),
                     "You are about to overwrite \"" + workingFile.getName() + "\" with new changes.\nAre you sure you want to save?",
                     "Save?",
                     JOptionPane.YES_NO_OPTION,
@@ -163,7 +163,7 @@ public class CemeteryPlotterMenu extends CemeteryPlotter implements ActionListen
                     null,
                     null);
 
-            if (open == JOptionPane.NO_OPTION) {
+            if (save == JOptionPane.NO_OPTION) {
                 return;
             }
         }
@@ -188,7 +188,7 @@ public class CemeteryPlotterMenu extends CemeteryPlotter implements ActionListen
         if (file != null) { // a file was selected
             try {
                 workingFile = file; // set the working file to the selected file
-                cemetery.save(file); // open the file using the cemetery object's load(file) method
+                cemetery.save(workingFile); // open the file using the cemetery object's load(file) method
                 cemetery.setModified(false);
                 cemeteryPlotterFrame.getFrame().setTitle("Cemetery Plotter (" + workingFile.getName() + ")");
             } catch (IOException ex) {
