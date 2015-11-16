0745dfca9fdf0b070ef3b7ffb8d6a242dc609d05
Daniel Kindler <DanielKindler@Dans-Mac.local>
2015-11-16 02:57:31 -0500

    saving now works with parse

--- Cemetery/src/cs/softengine/CemeteryPlotter.java
+++ Cemetery/src/cs/softengine/CemeteryPlotter.java
@@ -1,6 +1,8 @@
 package cs.softengine;
 
+import org.parse4j.ParseException;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * CemeteryPlotter utility for cemetery management.
@@ -14,7 +16,7 @@ public class CemeteryPlotter {
      * Main
      * @param args n/a
      */
-    public static void main(String[] args){
+    public static void main(String[] args) throws ParseException, FileNotFoundException {
         File defaultFile;
 
         switch (System.getProperty("os.name").toLowerCase()) {
@@ -27,5 +29,16 @@ public class CemeteryPlotter {
         workingFile = defaultFile; // set working file to default file
         cemetery = new Cemetery(workingFile); // initialize cemetery with default file
         cemeteryPlotterFrame = new CemeteryPlotterFrame(); // create and initialize GUI
+
+        ParseClient pc = new ParseClient();
+
+
+        try {
+            pc.saveFile(workingFile);
+        } catch (ParseException e) {
+            System.out.println("Exception thrown  :" + e);
+        } catch (FileNotFoundException e) {
+            System.out.println("Exception thrown  :" + e);
+        }
     }
 }
--- Cemetery/src/cs/softengine/ParseClient.java
+++ Cemetery/src/cs/softengine/ParseClient.java
@@ -2,13 +2,15 @@ package cs.softengine;
 import org.parse4j.*;
 import org.parse4j.callback.*;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.nio.charset.*;
+import java.util.Scanner;
 
 /**
  * Created by DanielKindler on 11/8/15.
  */
 public class ParseClient {
-    private static String FILE_OBJ_ID = "rQupr9UOFz";
+    private static String FILE_OBJ_ID = "cSRdwJXtdV";
     private static ParseObject saveObject = ParseObject.createWithoutData("Save", FILE_OBJ_ID);
     private static String APP_ID = "PMjsWvJ0jF5SvX70J7n19JaJ40SwsRSDbSz2wedp";
     private static String APP_REST_ID = "eZZDX9qchn88zkLhh02Q8xRToOTzlfrSVBEzs7qM";
@@ -23,16 +25,15 @@ public class ParseClient {
      * @return boolean if user logged in successfully or not
      */
     public static File getFile() {
-
         ParseFile file = (ParseFile) saveObject.get("file");
         File f = null;
         file.getDataInBackground(new GetDataCallback() {
             public void done(byte[] data, ParseException e) {
                 if (e == null) {
                     String str = new String(data, StandardCharsets.UTF_8);
-                    //f = new File("cemetery.db", str);
+                    // do
                 } else {
-                    // throw error
+                    // do
                 }
             }
         });
@@ -45,16 +46,15 @@ public class ParseClient {
      * @param inFile the file to be saved
      * @return boolean if file saved successfully or not
      */
-    public boolean saveFile(File inFile) throws ParseException {
-        String str = inFile.toString();
+    public boolean saveFile(File inFile) throws ParseException, FileNotFoundException {
+        String str = new Scanner(inFile).useDelimiter("\\Z").next();
         byte[] data = str.getBytes();
         ParseFile file = new ParseFile(inFile.getName(), data);
+        file.save();
         saveObject.put("file", file);
         saveObject.saveInBackground();
-
         return true;
     }
-
-
-
 }
+
+
